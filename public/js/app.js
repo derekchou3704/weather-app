f85a16de8475139d6c807db4a37e9b1dc76914b7
DerekChou <derekchou3704@gmail.com>
2021-05-31 22:42:57 +0800

    Setting up the user interface and render the results on the page

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,29 @@
 console.log('Clientside javascript is loaded!')
 
-const queryString = '!'
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const msgOne = document.querySelector('#msg-1')
+const msgTwo = document.querySelector('#msg-2')
 
-fetch(`http://localhost:3000/weather?address=${queryString}`).then( res => {
-    res.json().then( data => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.placeName)
-            console.log(data.tempResponse)
-        }
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then( res => {
+        msgOne.textContent = 'Loading'
+        msgTwo.textContent = ''
+
+        res.json().then( data => {
+            if (data.error) {
+                msgOne.textContent = data.error
+            } else {
+                msgOne.textContent = data.placeName
+                msgTwo.textContent = data.tempResponse
+            }
+        })
     })
+
+    // console.log('testing!')
+    // console.log(location)
 })
