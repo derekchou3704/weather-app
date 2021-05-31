b8bba2717052f30817d444cd47bb0639f6f3972b
DerekChou <derekchou3704@gmail.com>
2021-05-31 13:05:35 +0800

    Reorginizing templates by setting the new view location and utilizing partials for similar pages

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define path for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Set up handlebars engine and view location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('/', (req, res) => {
@@ -23,7 +33,9 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is sth to help'
+        helpText: 'This is sth to help',
+        title: 'Help',
+        name: 'Derek Chou'
     })
 })
 
